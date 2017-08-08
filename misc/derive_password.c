41a66b1b1098abc4b488baabc88b77e7267168db
-TOSH- <tosh@t0x0sh.org>
2017-08-08 21:45:50 +0200

    Two programs to generate/derive passwords

---
+++ misc/derive_password.c
@@ -0,0 +1,138 @@
+/* gcc -O2 -Wall derive_password.c -o derive_password */
+/* Derive a password from a given one (use min/maj combinaisons + l33t) */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+
+int digit_swap(int c) {
+  if(c == 'a')
+    return '4';
+  if(c == 'e')
+    return '3';
+  if(c == 't')
+    return '7';
+  if(c == 'l')
+    return '1';
+  if(c == 's')
+    return '5';
+  return c;
+}
+
+int digit_swap_rev(int c) {
+  if(c == '4')
+    return 'a';
+  if(c == '3')
+    return 'e';
+  if(c == '7')
+    return 't';
+  if(c == '1')
+    return 'l';
+  if(c == '5')
+    return 's';
+  return c;
+}
+
+int next_password_digit(char *pwd, int len) {
+  int i, c;
+
+  i = len - 1;
+
+  while(i >= 0) {
+    c = digit_swap(pwd[i]);
+    if(c == pwd[i] && digit_swap_rev(c) != c)
+      break;
+
+    pwd[i] = digit_swap(pwd[i]);
+    i--;
+  }
+
+  if(i < 0)
+    return 0;
+
+  pwd[i] = digit_swap_rev(pwd[i]);
+
+  return 1;
+}
+
+void derive_password_digit(FILE *stream, char *pwd, int len) {
+  char *pwd_copy;
+  int i;
+
+  if((pwd_copy = strdup(pwd)) == NULL) {
+    perror("[-] Failed to allocate memory:");
+    exit(EXIT_FAILURE);
+  }
+
+  for(i = 0; i < len; i++) {
+    pwd_copy[i] = digit_swap(pwd_copy[i]);
+  }
+
+  do {
+    fprintf(stream, "%s\n", pwd_copy);
+  } while(next_password_digit(pwd_copy, len));
+
+  free(pwd_copy);
+}
+
+int next_password_maj(char *pwd, int len) {
+  int i;
+
+  i = len - 1;
+
+  while(i >= 0) {
+    if(isalpha(pwd[i])) {
+      if(islower(pwd[i])) {
+        break;
+      } else {
+        pwd[i] = tolower(pwd[i]);
+      }
+    }
+    i--;
+  }
+
+  if(i < 0)
+    return 0;
+
+  pwd[i] = toupper(pwd[i]);
+
+  return 1;
+}
+
+void derive_password_maj(FILE *stream, char *pwd, int len) {
+  int i;
+
+  for(i = 0; i < len; i++) {
+    if(isalpha(pwd[i])) {
+      pwd[i] = tolower(pwd[i]);
+    }
+  }
+
+  do {
+    derive_password_digit(stream, pwd, len);
+  } while(next_password_maj(pwd, len));
+
+}
+
+int main(int argc, char **argv) {
+  char *pwd;
+
+  if(argc != 2) {
+    printf("Usage : %s <password>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if((pwd = strdup(argv[1])) == NULL) {
+    perror("[-] Failed to allocate memory\n");
+    exit(EXIT_FAILURE);
+  }
+
+  derive_password_maj(stdout, pwd, strlen(pwd));
+
+  free(pwd);
+
+  return 0;
+}
---
+++ misc/gen_password.c
@@ -0,0 +1,102 @@
+/* gcc -O2 -Wall gen_password.c -o gen_password */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define CHARSET_MAJ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#define CHARSET_MIN "abcdefghijklmnopqrstuvwxyz"
+#define CHARSET_DIG "0123456789"
+#define CHARSET_SPE "&~\"#'{([-|`_\\^@])=}+/?,;.:!<>"
+
+struct charset {
+  const char *charset;
+  int positions[256];
+  int len;
+};
+
+/*
+ * Build the table for optimization.
+ * We can then, from a given charset letter,
+ * know its position in O(1).
+ */
+void build_charset(struct charset *charset_table, const char *charset) {
+  int i;
+
+  memset(charset_table, 0, sizeof(*charset_table));
+  charset_table->len = strlen(charset);
+
+  for(i = 0; i < charset_table->len; i++) {
+    charset_table->positions[(unsigned char)charset[i]] = i;
+  }
+
+  charset_table->charset = charset;
+}
+
+int password_next(struct charset *charset, char *pwd, int pwd_len) {
+  const char *charset_str;
+  int charset_len;
+  int *charset_positions;
+  int i;
+
+  charset_str = charset->charset;
+  charset_len = charset->len;
+  charset_positions = charset->positions;
+
+  i = pwd_len - 1;
+
+  while(pwd[i] == charset_str[charset_len - 1] && i >= 0) {
+    pwd[i] = charset_str[0];
+    i--;
+  }
+
+  if(i < 0)
+    return 0;
+
+  pwd[i] = charset_str[charset_positions[(unsigned char)pwd[i]] + 1];
+
+  return 1;
+}
+
+void gen_from_charset(FILE *stream, struct charset *charset, int len) {
+  char *password;
+  int i;
+
+  if((password = malloc(len + 1)) == NULL) {
+    perror("[-] Failed to alloc memory: ");
+    exit(EXIT_FAILURE);
+  }
+
+  for(i = 0; i < len; i++) {
+    password[i] = charset->charset[0];
+  }
+  password[len] = 0;
+
+  do {
+    fprintf(stream, "%s\n", password);
+  } while(password_next(charset, password, len));
+
+  free(password);
+}
+
+
+int main(int argc, char **argv) {
+  const char *charset_str = CHARSET_MAJ CHARSET_MIN CHARSET_DIG CHARSET_SPE;
+  struct charset charset;
+
+  if(argc != 2 && argc != 3) {
+    printf("Usage : %s <len> [<charset>]\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if(argc == 3) {
+    charset_str = argv[2];
+  }
+
+  build_charset(&charset, charset_str);
+
+  gen_from_charset(stdout, &charset, atoi(argv[1]));
+
+  return 0;
+}
